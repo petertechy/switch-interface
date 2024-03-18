65bf53f89bcd85bc2af0343763d7ba99ef6628c6
Olanrewaju <ikolabaolanrewaju@gmail.com>
2024-03-18 13:03:30 +0100

    Switch screen

---
+++ script.js
@@ -0,0 +1,40 @@
+document.getElementById("yearly").addEventListener("click", () => {
+  document
+    .querySelector(".monthly-section")
+    .setAttribute("style", "display: none");
+  document
+    .querySelector(".yearly-section")
+    .setAttribute("style", "display: block");
+  document
+    .querySelector("bt1")
+    .setAttribute(
+      "style",
+      "background-color: #776AEB; border: 1px solid #FB8321;"
+    );
+  document
+    .querySelector("bt2")
+    .setAttribute(
+      "style",
+      "background-color: transparent; border: 1px solid #776AEB;"
+    );
+});
+document.getElementById("monthly").addEventListener("click", () => {
+  document
+    .querySelector(".monthly-section")
+    .setAttribute("style", "display: block");
+  document
+    .querySelector(".yearly-section")
+    .setAttribute("style", "display: none");
+  document
+    .querySelector("bt2")
+    .setAttribute(
+      "style",
+      "background-color: #FB8321; border: 1px solid #776AEB;"
+    );
+  document
+    .querySelector("bt1")
+    .setAttribute(
+      "style",
+      "background-color: transparent; border: 1px solid #FB8321;"
+    );
+});
